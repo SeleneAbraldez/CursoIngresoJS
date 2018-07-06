5d8e74edc309b0659d3f919dd75f6ec21e8e03ae
Selene <seleabraldez@gmail.com>
2018-07-06 10:46:26 -0300

    Add files via upload
    Ejercicio 6, 7, 8, 9 y 10 - Segunda clase, parseInt y porcentaje. 6/7

---
+++ jsEntradaSalida-7.js
@@ -0,0 +1,64 @@
+/*Debemos lograr tomar Los numeros por ID ,
+transformarlos a enteros (parseInt),realizar la operación correcta y 
+mostrar el resulto por medio de "ALERT"
+ej.: "la Resta es 750"*/ 
+function sumar()
+{	
+	var numeroUno;
+	var numeroDos;
+	var resultado;
+
+	numeroUno = document.getElementById('numeroUno').value;
+	numeroUno = parseInt(numeroUno);
+	numeroDos = document.getElementById('numeroDos').value;
+	numeroDos = parseInt(numeroDos);
+
+	resultado = numeroUno + numeroDos ;
+	alert("La suma es " + resultado);
+}
+
+function restar()
+{
+	var numeroUno;
+	var numeroDos;
+	var resultado;
+
+	numeroUno = document.getElementById('numeroUno').value;
+	numeroUno = parseInt(numeroUno);
+	numeroDos = document.getElementById('numeroDos').value;
+	numeroDos = parseInt(numeroDos);
+
+	resultado = numeroUno - numeroDos ;
+	alert("La resta es " + resultado);
+}
+
+function multiplicar()
+{ 
+	var numeroUno;
+	var numeroDos;
+	var resultado;
+
+	numeroUno = document.getElementById('numeroUno').value;
+	numeroUno = parseInt(numeroUno);
+	numeroDos = document.getElementById('numeroDos').value;
+	numeroDos = parseInt(numeroDos);
+
+	resultado = numeroUno * numeroDos ;
+	alert("La multiplicación es " + resultado);
+}
+
+function dividir()
+{
+	var numeroUno;
+	var numeroDos;
+	var resultado;
+
+	numeroUno = document.getElementById('numeroUno').value;
+	numeroUno = parseInt(numeroUno);
+	numeroDos = document.getElementById('numeroDos').value;
+	numeroDos = parseInt(numeroDos);
+
+	resultado = numeroUno / numeroDos ;
+	alert("La division es " + resultado);			
+}
+
