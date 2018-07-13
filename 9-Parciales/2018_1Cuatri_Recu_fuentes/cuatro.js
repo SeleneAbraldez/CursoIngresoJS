4bcdc4ec8e760961b577686fc1bcc360be968f3c
Selene <seleabraldez@gmail.com>
2018-07-13 12:24:27 -0300

    if
    Eje revisar alerts

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,42 @@
+/*(IF)Pedir dos números y mostrar el resultado:
+Si son iguales los muestro concatenados. 
+Si el primero es mayor, los divido, 
+de lo contrario los sumo. 
+Si la suma es menor a 50 ,además de mostrar el resultado, muestro el mensaje 
+"la suma es xxx y es menor a 50".*/
 function mostrar()
 {
+	var num1;
+	var num2; 
+	var divi;
+	var sum;
+	var resu;
+
+	num1 = prompt("Ingrese su primer numero;");
+	num2 = prompt("Ingrese su segundo numero;");
+
+	if (num1 == num2) {
+		resu = num1 + num2;
+		alert("Su resultado es: " + resu);
+	} else {
+		num1 = parseInt(num1);
+		num2 = parseInt(num2);
+
+		if (num1 > num2) {
+			resu = num1/num2;
+			alert("Su resultado es: " + resu);
+		} else {
+			resu = num1+num2;
+			if (resu < 50) {
+				alert("La suma es " + resu + " y es menor a 50");
+			}  else {
+				alert("Su resultado es: " + resu);
+			}
+		}
+	}
+
+	
+
+
 
 }
