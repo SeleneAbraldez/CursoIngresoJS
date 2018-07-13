c220c85dca12d9721307eed85b5e5e33c02ecf9e
Selene <seleabraldez@gmail.com>
2018-07-13 12:11:12 -0300

    Tp Switch

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
@@ -1,4 +1,30 @@
+/*(SWITCH)pedir el ingreso de un día de la semana, 
+si es fin de semana mostrar " buen finde", 
+si es día hábil “ a trabajar”, 
+si no es un día valido, también informarlo, 
+usar una sola ventana alert.*/
 function mostrar()
 {
+	var dia;
+
+	dia = prompt("Ingrese un dia (s/mayusculas ni tildes): ");
+
+	switch(dia){
+		case "lunes":
+		case "martes":
+		case "miercoles":
+		case "jueves":
+		case "viernes":
+			alert("A trabajar!");
+			break;
+
+		case "sabado":
+		case "domingo":
+			alert("Buen finde!");
+			break;
+
+		default:
+			alert("Ingrese dia valido");
+	}
 
 }
