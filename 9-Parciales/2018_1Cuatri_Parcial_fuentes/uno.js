32c37c0b30842dceedaebf5425c20249762b06f3
Selene <seleabraldez@gmail.com>
2018-07-13 22:35:31 -0300

    PP- prompt, alert
    Ejercicio de Parcial numero 1, 5/7

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,12 @@
 
 function mostrar()
 {
+	var ancho;
+	var largo;
+	var perimetro;
 
+	ancho = prompt("Ingrese el ancho");
+	largo = prompt("Ingrese ellargo");
+	perimetro = ancho * largo;
+	alert(perimetro);
 }
