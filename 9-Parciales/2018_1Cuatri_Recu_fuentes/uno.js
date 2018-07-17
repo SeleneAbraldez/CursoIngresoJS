68107b1ae59388a4f0414bdc05cc9c694aa6e266
Selene <seleabraldez@gmail.com>
2018-07-17 17:52:27 -0300

    Resuelto 1,2&3- PC
    Ejercicios de parcial-recu, 1, 2 y 3

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,22 @@
+/*Realizar el algoritmo que pida la base y la altura de un triángulo 
+equilátero, informar la superficie y el perímetro en una sola ventana alert.*/
 
 function mostrar()
 {
+	var base;
+	var altura;
+	var superficie;
+	var perimetro;
+
+	base = prompt("Ingrese la base del triangulo equilatero: ");
+	base = parseInt(base);
+	altura = prompt("Ingrese la altura del triangulo equilatero: ");
+	altura = parseInt(altura);
+
+	superficie = base * altura;
+	perimetro = (base + altura)*2;
+
+
+	alert("La superficie del triangulo es de " + superficie + " y el perimetro es de " + perimetro);
 
 }
