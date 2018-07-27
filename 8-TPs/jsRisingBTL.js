de7d0d2c2c76df08771afbfa836a1beb878ea97f
Selene <seleabraldez@gmail.com>
2018-07-27 10:41:42 -0300

    TP 12 - Validacion while + switch
    Validación de datos, versión con switch en vez de solo while

--- 8-TPs/jsRisingBTL.js
+++ 8-TPs/jsRisingBTL.js
@@ -9,5 +9,65 @@ F.	Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para
  */
 function ComenzarIngreso () 
 {
+	var edad;
+	var sexo;
+	var estadoC;
+	var sueldoB;
+	var numLegajo;
+	var nacionalidad;
+
+	edad = prompt("Ingresar edad:");
+	while (edad<18 || edad>90){
+		edad = prompt("Edad no valida, ingresar edad entre 18 y 90 inclusive:");
+	}
+	document.getElementById('Edad').value = edad;
+
+	sexo = prompt("Ingresar sexo, 'M' o 'F':");
+	while (sexo != "M" && sexo != "F"){
+		sexo = prompt("Sexo no valido, ingresar 'F' o 'M'");
+	}
+	document.getElementById('Sexo').value = sexo;
+
+	estadoC = prompt("Ingresar numero para Estado Civil, 1-Solterx, 2-Casadx, 3-Divorciadx y 4-Viudx:"); //INTENTAR CON SWITCH! SE PUDOOO
+	while(estadoC != 1 && estadoC != 2 && estadoC != 3 && estadoC != 4){
+		estadoC = prompt("Estadp Civil no valido, ingresar numero para Estado Civil, 1-Solterx, 2-Casadx, 3-Divorciadx y 4-Viudx:");
+	}
+	switch(estadoC){
+		case 1:
+			document.getElementById('EstadoCivil').value = "Solterx";
+		case 2:
+			document.getElementById('EstadoCivil').value = "Casadx";
+		case 3:
+			document.getElementById('EstadoCivil').value = "Divorciadx";
+		default:
+			document.getElementById('EstadoCivil').value = "Viudx";
+	}
+	//document.getElementById('EstadoCivil').value = estadoC;
+
+	sueldoB = prompt ("Ingrese su Sueldo Bruto, no menor a 8000:")
+	while(sueldoB<8000){
+		sueldoB = prompt("Invalido, ingrese su Sueldo Bruto, no menor a 8000:");
+	}
+	document.getElementById('Sueldo').value = sueldoB;
+
+	numLegajo = prompt ("Ingresar su numero de legajo numérico de 4 cifras, sin ceros a la izquierda:");
+	while(numLegajo<999 || numLegajo > 9999){
+		numLegajo = prompt("Invalido, ngresar su numero de legajo numérico de 4 cifras, sin ceros a la izquierda:");
+	}	
+	document.getElementById('Legajo').value = numLegajo;
+
+	nacionalidad = prompt("Ingrese letra de nacionalidad, “A” para argentinx, “E” para extranjerx, “N” para nacionalizadx:"); //INTENTAR SWITCH VV
+	while(nacionalidad != "A" && nacionalidad != "E" && nacionalidad != "N"){
+		nacionalidad = prompt("Invalido, ingrese letra de nacionalidad, “A” para argentinx, “E” para extranjerx, “N” para nacionalizadx:");
+	}
+	switch(nacionalidad){
+		case 1:
+			document.getElementById('Nacionalidad').value = "Argentinx";
+		case 2:
+			document.getElementById('Nacionalidad').value = "Extranjerx";
+		default:
+			document.getElementById('Nacionalidad').value = "Nacionalizadx";
+	}
+	//document.getElementById('Nacionalidad').value = nacionalidad;
  
 }
