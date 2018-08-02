2a0f5305bfd6634ad8aeb1a322818894df25de0f
Selene <seleabraldez@gmail.com>
2018-08-01 21:42:27 -0300

    6, 7, 8 - PP completos
    tengo sueño así que van medio medio, igual esta comentado lo que podría mejorar en un futuro

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,80 @@
+/*Realizar el algoritmo que permita iterar el ingreso de dos datos, una letra y un número entre -100 y 100 (validar) hasta que el usuario quiera e informar al terminar el ingreso por document.write: 
+a) La cantidad de números pares. 
+b) La cantidad de números impares. 
+c) La cantidad de ceros. 
+d) El promedio de todos los números positivos ingresados. 
+e) La suma de todos los números negativos.
+f) El número y la letra del máximo y el mínimo.
+
+Testeo con los siguientes datos 
+( d , -150(mal) , -50(bien)) 
+( z , 0) 
+( g , 20) 
+( a , 150(mal) , 0(bien)) 
+( b , 3) 
+( k , 7)*/
+
 function mostrar()
 {
+	var letra;
+	var numero;
+	var respuesta = "si";
+	var pares = 0;
+	var impares = 0;
+	var ceros = 0;
+	var promPosi = 0;
+	var contadorPosi = 0;
+	var sumNega = 0;
+	var letraMax;
+	var numMax=-101;
+	var letraMin;
+	var numMin=101;
+
+
+	while(respuesta != "no"){
+		letra = prompt("Ingresar una letra:");
+
+		numero = prompt("Ingresar un numero entre -100 y 100:"); 
+		while (numero<-100 || numero>100){
+			numero = prompt ("Numero no valido, ingresar numero entre -100 y 100:");
+		}
+		numero = parseInt(numero);
+
+		if(numero>0){ //a
+			pares ++;
+			promPosi = promPosi + numero; //d
+			contadorPosi ++;
+		} else if (numero<0) { //b
+			impares ++;
+			sumNega = sumNega + numero; //e
+		} else { //c
+			ceros ++;
+		}
+
+		if(numero>numMax){
+			numMax = numero;
+			letraMax = letra;
+		}
+
+		if (numero<numMin) {
+			numMin = numero;
+			letraMin = letra;
+		};
+
+		respuesta = prompt("¿Desea continuar? Ingrese 'no' para salir")
+	}
+
+
+	document.write("<br>a) La cantidad de números pares es de " + pares + "<br>b) La cantidad de números impares es de " + impares + "<br>c) La cantidad de ceros es de " + ceros + "<br>d) El promedio de todos los números positivos ingresados es de " + promPosi/contadorPosi + "<br>e) La suma de todos los números negativos es de " + sumNega + "<br>f) El número y la letra del máximo: " + numMax + ", " + letraMax + " y el mínimo: " + numMin + ", " + letraMin);
 
 }
+
+/*
+PRUEBA
+a) La cantidad de números pares es de 3
+b) La cantidad de números impares es de 1
+c) La cantidad de ceros es de 2
+d) El promedio de todos los números positivos ingresados es de 10
+e) La suma de todos los números negativos es de -50
+f) El número y la letra del máximo: 20, g y el mínimo: -50, d
+*/
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -4,42 +4,18 @@ a) El promedio de las notas totales.
 b) La nota más baja y el sexo de esa persona. 
 c) La cantidad de varones que su nota haya sido mayor o igual a 6.*/
 
-
 function mostrar()
-{ /*
-	var nombre;
-	var edad;
-	var sexo;
-	var contador = 1;
-
-
-	while (contador<4){ 
-		nombre = prompt(contador + ". Ingresar nombre:");
-
-		edad = prompt(contador + ". Ingresar edad: ");
-		while(edad<0 || edad >100){
-			edad = prompt(contador + ". Invalido, ingresar edad valida: ");
-		}
-		edad = parseInt(edad);
-
-		sexo = prompt(contador + ". Ingresar sexo, 'f' o 'm': ");
-		while(sexo!= "m" && sexo !="f"){
-			sexo = prompt(contador + ". Invalido, ingresar sexo valido 'f' o 'm': ");
-		}
-		contador++;
-	} */
-
-	
+{ 
 	var nombre;
 	var nota;
 	var sexo;
 	var contador = 1;
 	var promedio = 0;
-	var notaBajo;
+	var notaBajo = 11; //pongo un 11 a sabiendas de que ninguna nota va a superar el once, pero deberia parchearlo con un if de forma mas correcta 
 	var sexoBajo;
 	var varonAprob = 0;
 
-	while (contador<4){ 
+	while (contador<6){ 
 		nombre = prompt(contador + ". Ingresar nombre:");
 
 		nota = prompt(contador + ". Ingresar nota: ");
@@ -49,27 +25,23 @@ function mostrar()
 		nota = parseInt(nota);
 		promedio = promedio + nota; //1
 
-		sexo = prompt(contador + ". Ingresar sexo, 'f' o 'm': ");
+		sexo = prompt(contador + ". Ingresar sexo, 'f' o 'm': "); 
 		while(sexo!= "m" && sexo !="f"){
 			sexo = prompt(contador + ". Invalido, ingresar sexo valido 'f' o 'm': ");
 		}
 
-		if (nota<notaBajo){
+		if (nota<notaBajo){ //2
 			notaBajo = nota;
 			sexoBajo = sexo;
 		}
 
-		if (sexo == "m" & nota>5){
+		if (sexo == "m" & nota>5){ //3
 			varonAprob ++;
 		}
-
-
-
 
 		contador++;
 	}
 
-	alert("1. El promedio de las notas es de: " + promedio + "\n 2. La nota mas baja es: "+  notaBajo + ". El sexo de la persona con la nota mas baja es: " + sexoBajo + "\n 3. La cantidad de varones aprobados es de: " + varonAprob);
-	
-
+	alert("1. El promedio de las notas es de: " + promedio/(contador-1) + "\n 2. La nota mas baja es: "+  notaBajo + ". El sexo de la persona con la nota mas baja es: " + sexoBajo + "\n 3. La cantidad de varones aprobados es de: " + varonAprob); 
+	//uso contador - 1 para el promedio porque inicialice oontador en 1 para que quede bonito el orden, si huebira utilizado solo para fines pra<cticos y no esteticos, el -1 no iria. Tambien podria poner el contador ++ al principio y ahorrarme este comnentario, pero recien ahora me di cuenta y me da fiaca cambiarlo :D
 }
